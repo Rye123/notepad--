0f973939a28f34c47aa023bf22f7a0c09de56f6a
Ryan Toh <ryantoh99@gmail.com>
2023-08-26 21:10:46 +0800

    refactor: fix titlebar code to be consistent

--- tui/Titlebar.go
+++ tui/Titlebar.go
@@ -16,63 +16,63 @@ func NewTitleBar(appstate *util.AppState) *TitleBar {
 	return &TitleBar{false, appstate}
 }
 
-func (el *TitleBar) Draw() {
-	if el.hidden {
+func (elem *TitleBar) Draw() {
+	if elem.hidden {
 		return
 	}
 
-	scr_w, scr_h := el.appstate.Screen.Size()
+	scr_w, scr_h := elem.appstate.Screen.Size()
 	scr_w--; scr_h--
 
-	filename := el.appstate.Filename
+	filename := elem.appstate.Filename
 	
 	if len(filename) == 0 {
 		// If filename not set, set temporary title
-		filename = util.GetTemporaryTitle(el.appstate.TextBuffer.String())
+		filename = util.GetTemporaryTitle(elem.appstate.TextBuffer.String())
 		if len(filename) == 0 {
 			filename = "Untitled"
-			el.appstate.FileModified = false
+			elem.appstate.FileModified = false
 		}
 	}
 	
-	if el.appstate.FileModified {
+	if elem.appstate.FileModified {
 		filename = "*" + filename
 	}
 
-	titleText := "🗒 " + filename + " - " + el.appstate.AppName
-	drawText(el.appstate.Screen, 0, TITLEBAR_STARTROW, len(titleText), TITLEBAR_STARTROW, el.appstate.BarStyle, titleText)
-	drawHorizontalLine(el.appstate.Screen, 0, scr_w, TITLEBAR_ENDROW, el.appstate.BarStyle)
+	titleText := "🗒 " + filename + " - " + elem.appstate.AppName
+	drawText(elem.appstate.Screen, 0, TITLEBAR_STARTROW, len(titleText), TITLEBAR_STARTROW, elem.appstate.BarStyle, titleText)
+	drawHorizontalLine(elem.appstate.Screen, 0, scr_w, TITLEBAR_ENDROW, elem.appstate.BarStyle)
 }
 
-func (el *TitleBar) IsActive() bool {
+func (elem *TitleBar) IsActive() bool {
 	return false
 }
 
-func (el *TitleBar) Focus() {
+func (elem *TitleBar) Focus() {
 	panic("not implemented")
 }
 
-func (el *TitleBar) Unfocus() {
+func (elem *TitleBar) Unfocus() {
 	panic("not implemented")
 }
 
-func (el *TitleBar) GetCursorIndex() int {
+func (elem *TitleBar) GetCursorIndex() int {
 	panic("not implemented")
 }
 
-func (el *TitleBar) SetCursorIndex(newIndex int) {
+func (elem *TitleBar) SetCursorIndex(newIndex int) {
 	panic("not implemented")
 }
 
-func (el *TitleBar) IsHidden() bool {
-	return el.hidden
+func (elem *TitleBar) IsHidden() bool {
+	return elem.hidden
 }
 
-func (el *TitleBar) Hide() {
-	el.hidden = true
+func (elem *TitleBar) Hide() {
+	elem.hidden = true
 }
 
-func (el *TitleBar) Show() {
-	el.hidden = false
+func (elem *TitleBar) Show() {
+	elem.hidden = false
 }
 
